8937220c7877cfa8acfb11461fe1fc7d5c6bd21c
JJ Merelo <jjmerelo@gmail.com>
2016-06-17 19:09:21 +0200

    Changing graph type
    Changes from commit d359c72 56e1e12 and closes #7
    
    I don't have the data for the other graphs, though

--- graphs.R
+++ graphs.R
@@ -1,13 +1,23 @@
+library(ggplot2)
+library(ggthemes)
+
 slices <- c(106, 59)
 lbls <- c("Yes", "No")
-pie(slices, labels = lbls)
-
+slices.df <- data.frame( real.world = lbls, number=slices)
+ggplot(slices.df,aes(x=real.world,y=number,fill=real.world))+geom_bar(stat="identity")+labs(x="Real world problems?")+theme_tufte()
+ggsave("realproblem.png",width=4,height=3)
+#pie(slices, labels = lbls)
 
 slices <- c(108, 35,10,12)
 lbls <- c("GA", "GP","DE","Others")
-pie(slices, labels = lbls)
+slices.df <- data.frame( real.world = lbls, number=slices)
+ggplot(slices.df,aes(x=real.world,y=number,fill=real.world))+geom_bar(stat="identity")+theme_tufte()+labs(x="Evolutionary algorithm type")
+ggsave("algorithmtype.png",width=4,height=3)
 
 
 slices <- c(1, 69,95)
 lbls <- c("Chapter", "Journal","Conference")
-pie(slices, labels = lbls)
+slices.df <- data.frame( real.world = lbls, number=slices)
+ggplot(slices.df,aes(x=real.world,y=number,fill=real.world))+geom_bar(stat="identity")+theme_tufte()+labs(x="Paper type")
+ggsave("papertype.png",width=4,height=3)
+
