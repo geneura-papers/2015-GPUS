56e1e129d80b53e966f6757e1ea2748ad20e05a9
Pablo García Sánchez <fergunet@gmail.com>
2016-06-17 18:35:25 +0200

    Added R code for generating pie graphs @JJ

---
+++ graphs.R
@@ -0,0 +1,13 @@
+slices <- c(106, 59)
+lbls <- c("Yes", "No")
+pie(slices, labels = lbls)
+
+
+slices <- c(108, 35,10,12)
+lbls <- c("GA", "GP","DE","Others")
+pie(slices, labels = lbls)
+
+
+slices <- c(1, 69,95)
+lbls <- c("Chapter", "Journal","Conference")
+pie(slices, labels = lbls)
